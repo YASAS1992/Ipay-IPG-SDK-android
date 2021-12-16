706215719a74659609c8f8bc1189e9dd7249786f
Yasas Weerasekara <wysn1992@gmail.com>
2021-12-16 14:02:05 +0530

    ipg gateway activity

---
+++ Ipay_IPG_SDK_android/src/main/java/com/ipay/ipg_sdk_android/ActPaymentGateway.java
@@ -0,0 +1,14 @@
+package com.ipay.ipg_sdk_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActPaymentGateway extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_payment_gateway);
+    }
+}
